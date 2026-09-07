ec16ecbe1f2c3a5ccdbaa0345dbce550ea0038a1
Pottaprashanth <prashanthpotta722@gmail.com>
2026-09-07 19:52:20 +0530

    mobile bill using constructor chaining

--- myfirstproject/src/constructors/MobileBill.java
+++ myfirstproject/src/constructors/MobileBill.java
@@ -3,7 +3,7 @@ package constructors;
 import java.util.Scanner;
 
 public class MobileBill {
-String mobileModel;
+	String mobileModel;
 	double price;
 	int quantity;
 	double delivaryCharge;
@@ -23,41 +23,35 @@ String mobileModel;
 
 	public MobileBill(double price, int quantity) {
 		this(price, quantity, 0);
-		
+
 		System.out.println("2-arg constructor called");
 	}
 
 	public MobileBill(double price, int quantity, double delivaryCharge) {
 
-		
-		this.price=price;
-		this.quantity=quantity;
-		this.delivaryCharge=delivaryCharge;
+		this.price = price;
+		this.quantity = quantity;
+		this.delivaryCharge = delivaryCharge;
 		mobileCost = price * quantity;
-		System.out.println("Mobile cost:"+mobileCost);
+		System.out.println("Mobile cost:" + mobileCost);
 		finalBill = mobileCost + delivaryCharge;
-		System.out.println("Final Bill:"+finalBill);
+		System.out.println("Final Bill:" + finalBill);
 		System.out.println("3-arg constructor called");
 	}
 
 	public static void main(String[] args) {
-     Scanner sc=new Scanner(System.in);
-     
-     
-     System.out.println("Enter Mobile Model:");
-     String m=sc.next();
-     System.out.println("Enter price of Mobile:");
-     double p=sc.nextDouble();
-     System.out.println("Enter quantity of Mobile:");
-     int q=sc.nextInt();
-     System.out.println("Enter Delivery charges:");
-     double d=sc.nextDouble();
-     MobileBill m1=new MobileBill(p,q,d);
-    
-  
-     
-     
-     
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Mobile Model:");
+		String m = sc.next();
+		System.out.println("Enter price of Mobile:");
+		double p = sc.nextDouble();
+		System.out.println("Enter quantity of Mobile:");
+		int q = sc.nextInt();
+		System.out.println("Enter Delivery charges:");
+		double d = sc.nextDouble();
+		MobileBill m1 = new MobileBill(p, q, d);
+
 	}
 
 }
